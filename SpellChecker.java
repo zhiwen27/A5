458daf5daad2a3367045896fc8d43dab51ac8007
Zhiwen Ji <zji@smith.edu>
2024-10-09 21:59:19 -0400

    need read file

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,63 +1,78 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class SpellChecker {
 
     public static void modeCommandLine(String inputLine){
         SpellDictionary dict = new SpellDictionary("words.txt");
         String[] input = inputLine.split(" ");
-            for(String s: input){
-                if (!dict.containsWord(s)){
-                    System.err.println("Not found: " + s);
-                    System.err.print("Suggestions: ");
-                    ArrayList<String> alternatives = dict.nearMisses(s);
-                    for(String a: alternatives){
-                        System.err.print(a + " ");
-                    }
-                    System.err.println();
+        for(String s: input){
+            if (!dict.containsWord(s)){
+                System.out.println("Not found: " + s);
+                System.out.print("Suggestions: ");
+                ArrayList<String> returning = dict.nearMisses(s);
+                if (returning.size() == 0){
+                    System.out.println("none");
                 }
                 else{
-                    System.err.println("'" + s + "' is spelled correctly.");
+                    for(String a: returning){
+                        System.out.print(a + " ");
+                    }
+                    System.out.println();
                 }
             }
+            else{
+                System.err.println("'" + s + "' is spelled correctly.");
+            }
+        }
     }
     public static void main(String[] args) {
         SpellDictionary dict = new SpellDictionary("words.txt");
-        String inputLine = "";
         if (args.length == 0) {
-            Scanner scanner = new Scanner(System.in);
-            String filename = scanner.nextLine();
             try {
-                scanner = new Scanner(new File(filename));
+                File file = new File("sonnet.txt");
+                Scanner sc = new Scanner(file);
                 // making sure that the ``Scanner`` skips over punctuation marks.
-            } catch (FileNotFoundException e) {
-                System.err.println("Cannot locate file.");
-                System.exit(-1);
-            }
-            while (scanner.hasNextLine()) {
-                inputLine += scanner.next() + " ";
-                String[] input = inputLine.split(" ");
-                for(String s: input){
-                    if (!dict.containsWord(s)){
-                        System.err.println("Not found: " + s);
-                        System.err.print("Suggestions: ");
-                        ArrayList<String> alternatives = dict.nearMisses(s);
-                        for(String a: alternatives){
-                            System.err.print(a + " ");
-                        }
-                        System.err.println();
+                Hashtable<String,Integer> inputStorage = new Hashtable<>();
+                while (sc.hasNextLine()) {
+                    String temp = sc.next();
+                    String cleanedTemp = temp.replaceAll("\\p{Punct}", "");
+                    if (inputStorage.containsKey(cleanedTemp)){
+                        inputStorage.put(cleanedTemp, inputStorage.get(cleanedTemp) + 1);
                     }
                     else{
-                        System.err.println("'" + s + "' is spelled correctly.");
+                        inputStorage.put(cleanedTemp,1);
                     }
                 }
+                Enumeration<String> enumeration = inputStorage.keys();
+                while(enumeration.hasMoreElements()){
+                    String s = enumeration.nextElement();
+                    if (!dict.containsWord(s)){
+                        System.out.println("Misspelled: " + s);
+                        System.out.print("Suggestions: ");
+                        ArrayList<String> returning = dict.nearMisses(s);
+                        if (returning.size() == 0){
+                            System.out.print("none");
+                        }
+                        else{
+                            for(String a: returning){
+                                System.out.print(a + " ");
+                            }
+                            System.out.println();
+                        }
+                    }
+                }
+                sc.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Cannot locate file.");
+                System.exit(-1);
             }
-            scanner.close();
-        } else {
+        }
+        else {
             // Otherwise, echo what was read in for now
+            String inputLine = "";
             Scanner scanner = new Scanner(new StringReader(args[0]));
             while (scanner.hasNext()) {
                 inputLine += scanner.next() + " ";
