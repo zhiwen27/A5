a20e2bcf16470b2eb66c71d044667f185cb96429
Zhiwen Ji <zji@smith.edu>
2024-10-17 14:04:50 -0400

    without '

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,7 +11,7 @@ public class SpellChecker {
     public static void modeCommandLine(String inputLine){
         SpellDictionary dict = new SpellDictionary("words.txt");
         // ignore all the punctuations except for " ' "
-        String cleanedInputLine = inputLine.replaceAll("(?!['])\\p{Punct}", "");
+        String cleanedInputLine = inputLine.replaceAll("(\\p{Punct}", "");
         if (!dict.containsWord(cleanedInputLine)){
             System.out.println("Not found: " + cleanedInputLine);
             System.out.print("Suggestions: ");
